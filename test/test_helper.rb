66196dae52f801e4e5c04a05f49b67331e103ec8
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-05-03 19:23:28 +0800

    Enhance test setup by integrating DatabaseCleaner

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/spec"
+require "database_cleaner/active_record"
 
 module ActiveSupport
   class TestCase
@@ -11,5 +13,16 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    DatabaseCleaner.strategy = :transaction
+
+    class Minitest::Spec
+      before :each do
+        DatabaseCleaner.start
+      end
+
+      after :each do
+        DatabaseCleaner.clean
+      end
+    end
   end
 end
