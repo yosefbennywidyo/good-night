528ee88356070c13583ad7acf4d5a138f215d9ba
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-05-03 21:51:50 +0800

    Add user follow and follower relationships with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+  # Followers relationship
+  has_many :active_followings, class_name: "Following", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_followings, class_name: "Following", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_followings, source: :followed
+  has_many :followers, through: :passive_followings, source: :follower
+
+  validates :name, presence: true, uniqueness: true
+
+  def follow(other_user)
+    active_followings.create(followed_id: other_user.id) unless self == other_user
+  end
+
+  def unfollow(other_user)
+    active_followings.find_by(followed_id: other_user.id)&.destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
