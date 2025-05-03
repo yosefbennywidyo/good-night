b4479c532f89132a70870c74a35d50ff7f71c041
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-05-03 19:27:05 +0800

    Move API routes for users to a dedicated file and simplify main routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :users
-    end
-  end
+  draw(:api)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
---
+++ config/routes/api.rb
@@ -0,0 +1,5 @@
+namespace :api do
+  namespace :v1 do
+    resources :users
+  end
+end
