7ef7503de5a95e13773af253bf1d566b586a258b
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-05-04 11:20:08 +0800

    Update database schema for cache and queue; add following, followers, and sleep records counts to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_03_135946) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_04_014111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,6 +41,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_03_135946) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "following_count"
+    t.integer "followers_count"
+    t.integer "sleep_records_count"
     t.index ["name"], name: "index_users_on_name"
   end
 
